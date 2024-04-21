9c3cada2d63c7a4bcce678ff650188180f4e3ca2
DevWeb13 <giuliano.loic@sfr.fr>
2024-04-21 21:23:22 +0200

    Creating the dashboard layout

---
+++ src/components/ui/sidenav.tsx
@@ -0,0 +1,30 @@
+import { component$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+import { LRDQwikLogo } from "~/assets/svg/LRDQwikLogo";
+import { HiPowerOutline } from "@qwikest/icons/heroicons";
+import { NavLinks } from "./dashboard/nav-links";
+
+export const SideNav = component$(() => {
+  return (
+    <div class="flex h-full flex-col px-3 py-4 md:px-2">
+      <Link
+        class="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        href="/"
+      >
+        <div class="w-32 text-white md:w-40">
+          <LRDQwikLogo />
+        </div>
+      </Link>
+      <div class="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <NavLinks />
+        <div class="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form>
+          <button class="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <HiPowerOutline class="w-6" />
+            <div class="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+});
