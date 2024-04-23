08c42b52d93d5460787298438b31474a57b5b5f2
DevWeb13 <giuliano.loic@sfr.fr>
2024-04-23 16:40:26 +0200

    The <Link> component

--- src/components/ui/dashboard/nav-links.tsx
+++ src/components/ui/dashboard/nav-links.tsx
@@ -8,6 +8,7 @@ import {
   HiDocumentDuplicateOutline,
 } from "@qwikest/icons/heroicons";
 
+import { Link } from "@builder.io/qwik-city";
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -26,14 +27,14 @@ export const NavLinks = component$(() => {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
             class="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon class="w-6" />
             <p class="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
