38bf52d366f99eac3a099807a42c538d5d32cec4
DevWeb13 <giuliano.loic@sfr.fr>
2024-04-29 14:16:51 +0200

    test with all data

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,35 +1,30 @@
 import { component$ } from "@builder.io/qwik";
-import { useFetchRevenue } from "./layout";
+import {
+  useFetchCardData,
+  useFetchLatestInvoices,
+  useFetchRevenue,
+} from "./layout";
 import { RevenueChart } from "~/components/ui/dashboard/revenue-chart";
-// import { Card } from "~/components/ui/dashboard/cards";
-// import { RevenueChart } from "~/components/ui/dashboard/revenue-chart";
-// import LatestInvoices from "~/ui/dashboard/latest-invoices";
-
-// import {
-//   useFetchCardData,
-//   useFetchLatestInvoices,
-//   useFetchRevenue,
-// } from "./layout";
-// import { LatestInvoices } from "~/components/ui/dashboard/latest-invoices";
-// import { Card } from "~/components/ui/dashboard/cards";
+import { Card } from "~/components/ui/dashboard/cards";
+import { LatestInvoices } from "~/components/ui/dashboard/latest-invoices";
 
 export default component$(() => {
   const revenue = useFetchRevenue().value;
   console.log("revenue", revenue);
 
-  // const latestInvoices = useFetchLatestInvoices().value;
+  const latestInvoices = useFetchLatestInvoices().value;
 
-  // const {
-  //   numberOfInvoices,
-  //   numberOfCustomers,
-  //   totalPaidInvoices,
-  //   totalPendingInvoices,
-  // } = useFetchCardData().value;
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = useFetchCardData().value;
 
   return (
     <main>
       <h1 class={`lusitana mb-4 text-xl md:text-2xl`}>Dashboard</h1>
-      {/* <div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <div class="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
@@ -38,10 +33,10 @@ export default component$(() => {
           value={numberOfCustomers}
           type="customers"
         />
-      </div> */}
+      </div>
       <div class="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
