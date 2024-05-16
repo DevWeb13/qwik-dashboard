8e622c7f6817dbe85d438d3201a2ee6bc4b8eab8
DevWeb13 <giuliano.loic@sfr.fr>
2024-05-16 13:02:05 +0200

    Delete SSG

--- adapters/vercel-edge/vite.config.ts
+++ adapters/vercel-edge/vite.config.ts
@@ -11,12 +11,6 @@ export default extendConfig(baseConfig, () => {
       },
       outDir: ".vercel/output/functions/_qwik-city.func",
     },
-    plugins: [vercelEdgeAdapter( {
-      ssg: {
-          include: ['/'],
-          origin: 'https://qwik-dashboard.vercel.app',
-          sitemapOutFile: 'sitemap.xml'
-        }
-      })],
+    plugins: [vercelEdgeAdapter()],
   };
 });
