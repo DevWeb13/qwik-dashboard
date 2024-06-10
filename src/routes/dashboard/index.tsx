41f885d9d3757c4c6c16ac6c3bec1bfe1cc316f2
DevWeb13 <giuliano.loic@sfr.fr>
2024-06-11 00:25:55 +0200

    Practice: Streaming <LatestInvoices>

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -4,22 +4,16 @@ import { Resource, component$, useResource$ } from "@builder.io/qwik";
 import { RevenueChart } from "~/components/ui/dashboard/revenue-chart";
 import { Card } from "~/components/ui/dashboard/cards";
 import { LatestInvoices } from "~/components/ui/dashboard/latest-invoices";
-import { fetchCardData, fetchLatestInvoices } from "~/lib/data";
+import { fetchCardData } from "~/lib/data";
 import { DashboardSkeleton } from "~/components/ui/skeletons";
 
 export default component$(() => {
   const dataResource = useResource$(async ({ cleanup }) => {
-    // A good practice is to use `AbortController` to abort the fetching of data if
-    // new request comes in. We create a new `AbortController` and register a `cleanup`
-    // function which is called when this function re-runs.
     const controller = new AbortController();
     cleanup(() => controller.abort());
 
-    const [latestInvoices, cardData] = await Promise.all([
-      fetchLatestInvoices(),
-      fetchCardData(),
-    ]);
-    return { latestInvoices, cardData };
+    const cardData = await fetchCardData();
+    return { cardData };
   });
 
   return (
@@ -27,7 +21,7 @@ export default component$(() => {
       <h1 class="lusitana mb-4 text-xl md:text-2xl">Dashboard</h1>
       <Resource
         value={dataResource}
-        onResolved={({ latestInvoices, cardData }) => {
+        onResolved={({ cardData }) => {
           const {
             totalPaidInvoices,
             totalPendingInvoices,
@@ -60,7 +54,7 @@ export default component$(() => {
               </div>
               <div class="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <RevenueChart />
-                <LatestInvoices latestInvoices={latestInvoices} />
+                <LatestInvoices />
               </div>
             </>
           );
