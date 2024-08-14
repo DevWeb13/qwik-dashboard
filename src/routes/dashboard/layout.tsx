36846e6c5f1809c627c695b542a072ac34eeebb6
DevWeb13 <giuliano.loic@sfr.fr>
2024-08-14 09:26:22 +0200

    Add log

--- src/routes/dashboard/layout.tsx
+++ src/routes/dashboard/layout.tsx
@@ -2,8 +2,14 @@
 
 import { component$, Slot } from "@builder.io/qwik";
 import { SideNav } from "~/components/ui/dashboard/sidenav";
+import { isBrowser } from "@builder.io/qwik/build";
 
 export default component$(() => {
+  if (isBrowser) {
+    console.log("Exécution côté client");
+  } else {
+    console.log("Exécution côté serveur");
+  }
   return (
     <div class="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div class="w-full flex-none md:w-64">
