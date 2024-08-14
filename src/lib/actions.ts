6b3debe04797ec55533cc214269816c273887546
DevWeb13 <giuliano.loic@sfr.fr>
2024-08-14 11:17:35 +0200

    Storing values in cents

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,6 +3,7 @@
 import { server$ } from "@builder.io/qwik-city";
 
 export const createInvoice = server$(async function (data: { customerId: string, amount: number, status: string }) {
+  const amountInCents = Math.round(data.amount * 100);
   console.log(data);
   return  data.amount, data.customerId, data.status ;
 });
