b2cb612d1ea2337d1404a69ff6cb8090f6f0a613
DevWeb13 <giuliano.loic@sfr.fr>
2024-08-14 11:18:44 +0200

    Creating new dates

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,6 +4,7 @@ import { server$ } from "@builder.io/qwik-city";
 
 export const createInvoice = server$(async function (data: { customerId: string, amount: number, status: string }) {
   const amountInCents = Math.round(data.amount * 100);
-  console.log(data);
-  return  data.amount, data.customerId, data.status ;
+  const date = new Date().toISOString().split('T')[0];
+  
+ 
 });
